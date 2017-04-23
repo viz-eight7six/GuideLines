d16145f58a1327ae3c9b81bb0a4921a1874afcf8
Vinson Chen <viz@Vinsons-MacBook-Pro.local>
2017-04-22 23:14:35 -0700

    change styles

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -11,18 +11,18 @@ const App = ({ children }) => (
   <section className='whole'>
     <header>
       <nav className='main-nav'>
-        <nav className="left-nav">
-          <ul>
-            <Link to='/'>
-              <div className='logo'/>Guidelines</Link>
-            <SearchBarContainer/>
-          </ul>
-        </nav>
         <nav className='bottom-nav'>
           <label className="btn">
             <input type="checkbox" id="btnControl" onClick={createGuide}/>
             Create Guide!</label>
         </nav>
+          <nav className="left-nav">
+            <ul>
+              <Link to='/'>
+                <div className='logo'/>Guidelines</Link>
+              </ul>
+            </nav>
+            <SearchBarContainer/>
         <nav className="right-nav">
           <GreetingContainer />
         </nav>
